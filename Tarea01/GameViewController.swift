e4a47039c7a301caa3212a72528a2c222140a209
lars <larsbiegstraaten@gmail.com>
2023-10-18 09:11:35 +0100

    Programmed the image view and made a list with the pictures to show

--- Tarea01/GameViewController.swift
+++ Tarea01/GameViewController.swift
@@ -3,10 +3,13 @@ import UIKit
 
 class GameViewController: UIViewController {
 
+    @IBOutlet weak var bigImageView: UIImageView!
+    let ImagesList = [UIImage(named: "blastoise"), UIImage(named: "charmander"), UIImage(named: "BULBASUR"),UIImage(named: "DRATINI"), UIImage(named: "EVEE"), UIImage(named: "meowth")]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        bigImageView.image = ImagesList[0]
+        
     }
     
 
