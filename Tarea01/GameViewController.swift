f889e2f13f7d96e0f1add620d730ffd87341bde3
lars <larsbiegstraaten@gmail.com>
2023-10-18 10:18:48 +0100

    Made the function that uses a timer of 2 seconds

--- Tarea01/GameViewController.swift
+++ Tarea01/GameViewController.swift
@@ -5,12 +5,25 @@ class GameViewController: UIViewController {
 
     @IBOutlet weak var bigImageView: UIImageView!
     let ImagesList = [UIImage(named: "blastoise"), UIImage(named: "charmander"), UIImage(named: "BULBASUR"),UIImage(named: "DRATINI"), UIImage(named: "EVEE"), UIImage(named: "meowth")]
-    
+    var ImagesListShuffled: [UIImage?]?
+    var timer: Timer?
     override func viewDidLoad() {
         super.viewDidLoad()
-        bigImageView.image = ImagesList[0]
+       // let numbers = [0, 1, 2, 3, 4, 5]
+        ImagesListShuffled = ImagesList.shuffled()
+        timer = Timer.scheduledTimer(timeInterval: 2, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+        
+            
+        
+
+    }
+    
+    @objc func updateTimer(){
         
     }
     
+    func randomizeOrder() {
+
+    }
 
 }
