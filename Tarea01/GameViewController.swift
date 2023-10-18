fd01be1f44267de180fc32fb37f5b84cb3bf418b
lars <larsbiegstraaten@gmail.com>
2023-10-18 11:19:58 +0100

    cleaned the code

--- Tarea01/GameViewController.swift
+++ Tarea01/GameViewController.swift
@@ -13,26 +13,15 @@ class GameViewController: UIViewController {
         let numbers = [0, 1, 2, 3, 4, 5]
         numbersShuffled = numbers.shuffled()
         timer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
-        
-            
-        
-
     }
     
     @objc func updateTimer(){
         print(counter)
         print(numbersShuffled![counter])
         bigImageView.image = ImagesList[numbersShuffled![counter]]
-        counter += 1
-        
+        counter += 1        
         if counter == 6 {
             timer?.invalidate()
         }
-
     }
-    
-    func randomizeOrder() {
-
-    }
-
 }
