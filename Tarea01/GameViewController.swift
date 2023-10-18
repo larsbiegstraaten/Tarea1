4d93441f069083fa917d645d048945e5b49da365
lars <larsbiegstraaten@gmail.com>
2023-10-18 11:16:15 +0100

    Random images shown and recorded the position in a array

--- Tarea01/GameViewController.swift
+++ Tarea01/GameViewController.swift
@@ -5,14 +5,13 @@ class GameViewController: UIViewController {
 
     @IBOutlet weak var bigImageView: UIImageView!
     let ImagesList = [UIImage(named: "blastoise"), UIImage(named: "charmander"), UIImage(named: "BULBASUR"),UIImage(named: "DRATINI"), UIImage(named: "EVEE"), UIImage(named: "meowth")]
-    var ImagesListShuffled: [UIImage?]?
     var timer: Timer?
-    var numbersShowed: [Int]?
     var counter: Int = 0
+    var numbersShuffled: [Int]?
     override func viewDidLoad() {
         super.viewDidLoad()
-       // let numbers = [0, 1, 2, 3, 4, 5]
-        ImagesListShuffled = ImagesList.shuffled()
+        let numbers = [0, 1, 2, 3, 4, 5]
+        numbersShuffled = numbers.shuffled()
         timer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
         
             
@@ -21,14 +20,15 @@ class GameViewController: UIViewController {
     }
     
     @objc func updateTimer(){
-        let randomInt = Int.random(in: 0..<6)
-        
-        bigImageView.image = ImagesListShuffled![randomInt]
-        numbersShowed?.append(randomInt)
+        print(counter)
+        print(numbersShuffled![counter])
+        bigImageView.image = ImagesList[numbersShuffled![counter]]
         counter += 1
+        
         if counter == 6 {
             timer?.invalidate()
         }
+
     }
     
     func randomizeOrder() {
