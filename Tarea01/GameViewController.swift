5b0aa6462865a963e55503ad43b8eed3f0f407f2
lars <larsbiegstraaten@gmail.com>
2023-10-18 10:45:41 +0100

    Updated timer function and now 6 pictures in random order show in the view

--- Tarea01/GameViewController.swift
+++ Tarea01/GameViewController.swift
@@ -7,11 +7,13 @@ class GameViewController: UIViewController {
     let ImagesList = [UIImage(named: "blastoise"), UIImage(named: "charmander"), UIImage(named: "BULBASUR"),UIImage(named: "DRATINI"), UIImage(named: "EVEE"), UIImage(named: "meowth")]
     var ImagesListShuffled: [UIImage?]?
     var timer: Timer?
+    var numbersShowed: [Int]?
+    var counter: Int = 0
     override func viewDidLoad() {
         super.viewDidLoad()
        // let numbers = [0, 1, 2, 3, 4, 5]
         ImagesListShuffled = ImagesList.shuffled()
-        timer = Timer.scheduledTimer(timeInterval: 2, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+        timer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
         
             
         
@@ -19,7 +21,14 @@ class GameViewController: UIViewController {
     }
     
     @objc func updateTimer(){
+        let randomInt = Int.random(in: 0..<6)
         
+        bigImageView.image = ImagesListShuffled![randomInt]
+        numbersShowed?.append(randomInt)
+        counter += 1
+        if counter == 6 {
+            timer?.invalidate()
+        }
     }
     
     func randomizeOrder() {
